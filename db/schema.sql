c033c03ea4ab5c305a29f6603baef9717fb92a8c
WillV1 <wmvanhook@gmail.com>
2020-05-31 15:09:42 -0400

    Pushing intitial files to Github

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS burgers_DB;
+CREATE database burgers_DB;
+
+USE burgers_DB;
+
+CREATE TABLE burgers (
+  id INT NOT NULL AUTO_INCREMENT,
+  burger_name VARCHAR(255) NOT NULL,
+  devoured BOOLEAN NOT NULL,
+  PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+/* Seeds for SQL table. We haven't discussed this type of file yet */
+USE burgers_DB;
+
+/* Insert Rows into new table */
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Double Stack", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Whopper", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("All-American Burger", false);
