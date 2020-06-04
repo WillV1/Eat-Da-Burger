cc0fe68cce75cb70df7bf2d46b66827cf57f523b
WillV1 <wmvanhook@gmail.com>
2020-06-04 08:17:30 -0400

    Uploading updated files for app part 2

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 8080;
 
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static("./public/assets/img"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
